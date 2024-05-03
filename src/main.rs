859c5299dd1321a133fb2d25631515c91e4c499d
Luka Colić <lcenzo@protonmail.ch>
2024-05-03 22:32:58 +0200

    Implement returning empty 200 response to complete 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn return_empty_200(stream: &mut TcpStream) {
+    let status_line = b"HTTP/1.1 200 OK";
+    let headers = b""; //b"Content-Length: 0\r\nConnection: close\r\n";
+    let message_body = b"";
+
+    let buf: [&[u8]; 5] = [status_line, b"\r\n", headers, b"\r\n", message_body];
+    let buf = buf.concat();
+
+    if let Err(err) = stream.write_all(buf.as_slice()) {
+        eprintln!("return_empty_200 failed due to {err}");
+    }
+
+    if let Err(err) = stream.flush() {
+        eprintln!("TCP stream flush failed in return_empty_200: {err}");
+    }
+}
 
 fn main() {
     println!("Test print for logging.");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+    let listener = TcpListener::bind("127.0.0.1:4221")
+        .expect("Expected to be able to bind TCP listener to port 4221");
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                println!("Accepted new connection");
+                return_empty_200(&mut _stream);
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error: {}", e);
             }
         }
     }
