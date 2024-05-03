fd4f13ed93d0a8da07dfb33629e46dc1829fe1d7
Luka Colić <lcenzo@protonmail.ch>
2024-05-03 23:26:31 +0200

    Respond based on path, complete part 3

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,54 @@
+use std::io::Read;
 use std::{
     io::Write,
     net::{TcpListener, TcpStream},
 };
 
-fn return_empty_200(stream: &mut TcpStream) {
-    let status_line = b"HTTP/1.1 200 OK";
-    let headers = b""; //b"Content-Length: 0\r\nConnection: close\r\n";
+fn return_empty_http_response(stream: &mut TcpStream, code: u32, phrase: &[u8]) {
+    let status_line = [b"HTTP/1.1 ", code.to_string().as_bytes(), b" ", phrase].concat();
+    let headers = b"Content-Length: 0\r\nConnection: close\r\n";
     let message_body = b"";
 
-    let buf: [&[u8]; 5] = [status_line, b"\r\n", headers, b"\r\n", message_body];
+    let buf: [&[u8]; 5] = [
+        status_line.as_slice(),
+        b"\r\n",
+        headers,
+        b"\r\n",
+        message_body,
+    ];
     let buf = buf.concat();
 
     if let Err(err) = stream.write_all(buf.as_slice()) {
-        eprintln!("return_empty_200 failed due to {err}");
+        eprintln!("return_empty_404 failed due to {err}");
+        return;
     }
 
     if let Err(err) = stream.flush() {
-        eprintln!("TCP stream flush failed in return_empty_200: {err}");
+        eprintln!("TCP stream flush failed in return_empty_404: {err}");
+    }
+}
+
+fn respond_via_http(stream: &mut TcpStream) {
+    let mut buf: [u8; 1024] = [0; 1024];
+    if let Err(err) = stream.read(&mut buf) {
+        eprintln!("Error when reading string from TCP stream: {err}");
+        return;
+    }
+
+    let buf = String::from_utf8_lossy(&buf);
+
+    let path = buf
+        .lines()
+        .next()
+        .and_then(|start_line| start_line.split_whitespace().nth(1))
+        .unwrap_or_else(|| {
+            eprintln!("Unable to parse path from TCP stream, defaulting to /");
+            "/"
+        });
+
+    match path.trim() {
+        "/" => return_empty_http_response(stream, 200, b"OK"),
+        _ => return_empty_http_response(stream, 404, b"Not Found"),
     }
 }
 
@@ -30,7 +62,7 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("Accepted new connection");
-                return_empty_200(&mut _stream);
+                respond_via_http(&mut _stream);
             }
             Err(e) => {
                 println!("Error: {}", e);
