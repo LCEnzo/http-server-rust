e20321040640553bf7550c929e5d7df7d62b698b
Luka Colić <lcenzo@protonmail.ch>
2024-05-04 11:40:20 +0200

    Return paths, finish stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::str::FromStr;
 use std::{
     io::Write,
     net::{TcpListener, TcpStream},
@@ -28,6 +29,37 @@ fn return_empty_http_response(stream: &mut TcpStream, code: u32, phrase: &[u8]) 
     }
 }
 
+fn return_path_http_response(stream: &mut TcpStream, path: &mut String) {
+    if !path.starts_with("/echo/") {
+        return_empty_http_response(stream, 400, b"Bad Request");
+        return;
+    }
+
+    let path = path.split_off(6);
+    let len = path.len();
+
+    let status_line = b"HTTP/1.1 200 OK".as_slice();
+    let headers = [
+        b"Content-Length: ",
+        len.to_string().as_bytes(),
+        b"\r\nConnection: close\r\nContent-Type: text/plain\r\n",
+    ]
+    .concat();
+    let headers = headers.as_slice();
+    let message_body = path.as_bytes();
+
+    let buf = [status_line, b"\r\n", headers, b"\r\n", message_body].concat();
+
+    if let Err(err) = stream.write_all(buf.as_slice()) {
+        eprintln!("return_empty_404 failed due to {err}");
+        return;
+    }
+
+    if let Err(err) = stream.flush() {
+        eprintln!("TCP stream flush failed in return_empty_404: {err}");
+    }
+}
+
 fn respond_via_http(stream: &mut TcpStream) {
     let mut buf: [u8; 1024] = [0; 1024];
     if let Err(err) = stream.read(&mut buf) {
@@ -44,10 +76,14 @@ fn respond_via_http(stream: &mut TcpStream) {
         .unwrap_or_else(|| {
             eprintln!("Unable to parse path from TCP stream, defaulting to /");
             "/"
-        });
+        })
+        .trim();
+    let mut path: String =
+        String::from_str(path).expect("Expected to be able to convert path from &str to String");
 
-    match path.trim() {
+    match path.as_str() {
         "/" => return_empty_http_response(stream, 200, b"OK"),
+        _ if path.starts_with("/echo/") => return_path_http_response(stream, &mut path),
         _ => return_empty_http_response(stream, 404, b"Not Found"),
     }
 }
