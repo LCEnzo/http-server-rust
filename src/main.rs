8d16d8682aed735cc1e242aaf5d88dcaa1e8d53d
Luka Colić <lcenzo@protonmail.ch>
2024-05-05 14:44:04 +0200

    Refactor code to be async

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
-use std::io::Read;
 use std::str::FromStr;
-use std::{
-    io::Write,
-    net::{TcpListener, TcpStream},
-};
+use tokio::io::Result as TIOResult;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
 
 mod types;
 
 use types::{HttpMethod, HttpRequest, HttpResponse};
 
-fn return_http_response(stream: &mut TcpStream, resp: &HttpResponse) {
-    if let Err(err) = stream.write_all(resp.to_byte_string().as_slice()) {
+async fn return_http_response(stream: &mut TcpStream, resp: &HttpResponse) {
+    if let Err(err) = stream.write_all(resp.to_byte_string().as_slice()).await {
         eprintln!("return_empty_http_response failed due to {err}");
         return;
     }
 
-    if let Err(err) = stream.flush() {
+    if let Err(err) = stream.flush().await {
         eprintln!("TCP stream flush failed in return_empty_http_response: {err}");
     }
 }
 
-fn return_empty_http_response(stream: &mut TcpStream, status_code: u16, phrase: Vec<u8>) {
+async fn return_empty_http_response(stream: &mut TcpStream, status_code: u16, phrase: Vec<u8>) {
     let resp = HttpResponse {
         status_code,
         phrase,
         headers: vec![b"Content-Length: 0".to_vec(), b"Connection: close".to_vec()],
         body: vec![],
     };
-    return_http_response(stream, &resp)
+    
+    return_http_response(stream, &resp).await
 }
 
-fn return_path_http_response(stream: &mut TcpStream, path: &mut String) {
+async fn return_path_http_response(stream: &mut TcpStream, path: &mut String) {
     if !path.starts_with("/echo/") {
-        return_empty_http_response(stream, 400, b"Bad Request".to_vec());
+        return_empty_http_response(stream, 400, b"Bad Request".to_vec()).await;
         return;
     }
 
@@ -49,10 +49,11 @@ fn return_path_http_response(stream: &mut TcpStream, path: &mut String) {
         ],
         body: path.as_bytes().to_vec(),
     };
-    return_http_response(stream, &resp)
+
+    return_http_response(stream, &resp).await
 }
 
-fn return_user_agent_response(stream: &mut TcpStream, req: &HttpRequest) {
+async fn return_user_agent_response(stream: &mut TcpStream, req: &HttpRequest) {
     let user_agent = req
         .headers
         .get("User-Agent")
@@ -68,12 +69,13 @@ fn return_user_agent_response(stream: &mut TcpStream, req: &HttpRequest) {
         ],
         body: user_agent.into_bytes().to_vec(),
     };
-    return_http_response(stream, &resp)
+
+    return_http_response(stream, &resp).await
 }
 
-fn respond_via_http(stream: &mut TcpStream) {
+async fn respond_via_http(stream: &mut TcpStream) {
     let mut buf: [u8; 1024] = [0; 1024];
-    if let Err(err) = stream.read(&mut buf) {
+    if let Err(err) = stream.read(&mut buf).await {
         eprintln!("Error when reading string from TCP stream: {err}");
         return;
     }
@@ -83,37 +85,39 @@ fn respond_via_http(stream: &mut TcpStream) {
     if let Err(err) = request {
         eprintln!("err: {err}");
         eprintln!("Expected to successfully parse request into a HttpRequest struct");
-        return_empty_http_response(stream, 400, b"Bad Request".to_vec());
+        return_empty_http_response(stream, 400, b"Bad Request".to_vec()).await;
         return;
     }
     let request = request.unwrap();
 
     match request.path.as_str() {
-        "/" => return_empty_http_response(stream, 200, b"OK".to_vec()),
+        "/" => return_empty_http_response(stream, 200, b"OK".to_vec()).await,
         "/user-agent" if request.method == HttpMethod::Get => {
-            return_user_agent_response(stream, &request)
+            return_user_agent_response(stream, &request).await
         }
         _ if request.path.starts_with("/echo/") => {
-            return_path_http_response(stream, &mut request.path.clone())
+            return_path_http_response(stream, &mut request.path.clone()).await
         }
-        _ => return_empty_http_response(stream, 404, b"Not Found".to_vec()),
+        _ => return_empty_http_response(stream, 404, b"Not Found".to_vec()).await,
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221")
-        .expect("Expected to be able to bind TCP listener to port 4221");
+#[tokio::main]
+async fn main() -> TIOResult<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221").await?;
 
     println!("TcpListener has bound 4221.");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, _addr)) => {
                 println!("Accepted new connection");
-                respond_via_http(&mut _stream);
-            }
+                tokio::spawn(async move { 
+                    respond_via_http(&mut stream).await
+                });
+            },
             Err(e) => {
-                println!("Error: {}", e);
+                println!("Failed to accept connection: {}", e);
             }
         }
     }
